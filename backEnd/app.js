670d6c2074fe95519969751bf9d15bb6aee00a14
matthieu6125 <matth.lebreton61@gmail.com>
2024-07-13 16:56:08 +0200

    début de l'ajout de la fonction signin

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,24 +1,28 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+const userRoutes = require('./routes/user');
+const bodyParser = require('body-parser');
+
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+mongoose.connect('mongodb+srv://Matthieu:1234@cluster0.4szmqif.mongodb.net/',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use('/api/auth', userRoutes);
+
 
 module.exports = app;
